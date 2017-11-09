067c8daa652e16f9b4f4be32086ad7f59baf5b0b
Lucas Vieira Ribeiro <lucas.ribeiro@172-16-3-152.0000.epoa.br.iosda.org>
2017-11-09 10:48:32 -0200

    Tela basica

--- OverwatchChallenge/OverwatchChallenge/ViewController.swift
+++ OverwatchChallenge/OverwatchChallenge/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageIcon: UIImageView!
+    @IBOutlet weak var appName: UILabel!
+    @IBOutlet weak var userName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func buttonSearch(_ sender: Any) {
+    }
     //Teste
 
 
