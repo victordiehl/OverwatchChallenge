1b9f80320f027059b12ed84b50d60c549d6b8dd4
rangel <rangel@172-16-3-155.0000.epoa.br.iosda.org>
2017-11-09 10:44:01 -0200

    classe player

---
+++ OverwatchChallenge/OverwatchChallenge/Player.swift
@@ -0,0 +1,36 @@
+//
+//  Player.swift
+//  OverwatchChallenge
+//
+//  Created by rangel on 09/11/17.
+//  Copyright © 2017 victordiehl. All rights reserved.
+//
+
+import UIKit
+
+struct Player{
+    
+    var win_rate : Double
+    var avatar : UIImage
+    var losses : Int
+    var rank_image : UIImage
+    var ties : Int
+    var wins : Int
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+  
+
+}
